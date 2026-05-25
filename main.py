751ac9304d1dd48c421c59038e4138aa32801e64
Arthur <arthurvictor3434@gmail.com>
2026-05-24 23:53:54 -0300

    feat(main): implementa ponto de entrada para orquestrar busca e exportação
    - Cria o fluxo principal de execução do projeto utilizando blocos try e except
    - Integra a classe RequisitionBody com as funções get_csv e export_csv do api_service

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from datadengue!")
-
+from src.api_service import export_csv, get_csv
+from src.models import RequisitionBody
 
 if __name__ == "__main__":
-    main()
+    csv_api = RequisitionBody()
+
+    try:
+        print("Buscando dados na API...")
+        dataframe = get_csv(csv_api)
+        export_csv(dataframe, file_name="Dengue_Jundiai.csv")
+
+    except Exception as e:
+        print(f"Ocorreu um problema: {e}")
